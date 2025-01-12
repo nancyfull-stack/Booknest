72e861fe804d334cbb953d831e6d9519783c2c2a
nancyfull-stack <nancyirak291@gmail.com>
2025-01-12 07:46:52 +0000

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Header from "@/components/Header";
+import { HeroHighlight } from "@/components/Hero";
 
 function Page() {
   return (
     <div>
       <Header />
+      <HeroHighlight>
+        <h1 className="text-2xl sm:text-4xl md:text-6xl">BookNest</h1>
+      </HeroHighlight>
+      <p>Hey there</p>
     </div>
   );
 }
