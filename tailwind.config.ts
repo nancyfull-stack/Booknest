3d89918eba10d3b7294337cb6d04845c6aa8f0b7
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-01-13 18:09:04 +0000

    use cjs imports for type safety

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
-import svgToDataUri from "mini-svg-data-uri";
-import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 export default {
   darkMode: ["class"],
